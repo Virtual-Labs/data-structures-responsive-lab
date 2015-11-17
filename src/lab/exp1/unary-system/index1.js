b95938b0d0fb53bdf1aa71d080cad6998ea03485
khushpreet-kaur <khushpreetk363@gmail.com>
2015-11-17 16:20:02 +0530

    Implement unary multiplication with animation and styles

--- src/lab/exp1/unary-system/index1.js
+++ src/lab/exp1/unary-system/index1.js
@@ -36,31 +36,42 @@ function add(){
     } 
 }
 
-
-function mul(a,b)
+var  a, b;
+function mul()
 {
   a = document.getElementById('first').value;
   b = document.getElementById('second').value;
+  setTimeout(showMainLabel, 100);
+}
 
-  document.getElementById('third').innerHTML = a.length;
-  document.getElementById('fourth').innerHTML= b.length;
+function showMainLabel(){
+  document.getElementById("mulLabel").innerHTML = "Take a minimum count and perform repeated addition until count becomes 1.. ";
+  document.getElementById('val1Res').innerHTML = "First Value: " + a;
+  document.getElementById('val2Res').innerHTML= "First Value: " + b;
+  document.getElementById('mulCount1').innerHTML = "Count: " + a.length;
+  document.getElementById('mulCount2').innerHTML= "Count: " + b.length;
+  setTimeout(mulLogic, 1000);
+}
 
+function mulLogic() {
+    
     var count = b.length;
-    console.log("count" + count)
-    document.getElementById('fifth').innerHTML = count;
-     var res = ' ';
+    var res = ' ';
+
      while(count != 0)
      {
         res += a;
-        console.log("result is: " + res);
+        document.getElementById("mulCount").innerHTML = "Count is: " + count; 
+        console.log(res)
+        console.log(count);
         count--;
-     }
-     document.getElementById('seventh').innerHTML = res;
-  }
+
+     }     
+     document.getElementById("mulRes").innerHTML = res;
+     document.getElementById("labelRes").innerHTML = "Multiplication of two unary number is = " + res;
+   }
+
 
 function reset(){
   location.reload();
-  // document.getElementById('first').value = ""
-  // document.getElementById('second').value = ""
- 
 } 
