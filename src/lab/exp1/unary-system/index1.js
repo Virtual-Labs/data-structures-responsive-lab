78c0a79d5615a80d3580842ac29641091f4606f6
khushpreet-kaur <khushpreetk363@gmail.com>
2015-11-17 12:54:44 +0530

    Update unary multiplication function

--- src/lab/exp1/unary-system/index1.js
+++ src/lab/exp1/unary-system/index1.js
@@ -1,34 +1,25 @@
-// function add(a,b)
-// {
-//  a = document.getElementById('val1').value;
-//  b = document.getElementById('val2').value;
-
-//  var res = a.concat(b);
-//  document.getElementById('sixth').innerHTML = res;
-// }
-
-
 var a, b
  function showFistVal()
 {
  document.getElementById("val1Res").innerHTML = "First number = " + "  " + a;
- setTimeout(showSecondVal,1000);
+ setTimeout(showSecondVal,100);
 }
 
 function showSecondVal(){
  document.getElementById("val2Res").innerHTML = "Second number = " + "  " + b; 
- setTimeout(label,1000);
+ setTimeout(label,1800);
  
 }
 
 function label() {
   document.getElementById('labelRes').innerHTML = "First number is concatinating with second...";
-  setTimeout(concatRes, 1500);
+  setTimeout(concatRes, 1800);
 }
 
 function concatRes(){
     var res = a.concat(b);
     document.getElementById("concatResDiv").innerHTML = "Addition of two numbers is:  " + "  " + res;
+    setTimeout(reset, 3000);
 }
 
 function add(){
@@ -41,54 +32,35 @@ function add(){
     }
     else
     {
-      setTimeout(showFistVal,1000);
+      setTimeout(showFistVal,100);
     } 
 }
 
 
 function mul(a,b)
 {
+  a = document.getElementById('first').value;
+  b = document.getElementById('second').value;
 
- a = document.getElementById('first').value;
- b = document.getElementById('second').value;
+  document.getElementById('third').innerHTML = a.length;
+  document.getElementById('fourth').innerHTML= b.length;
 
- 
-
- document.getElementById('third').innerHTML = a.length;
- document.getElementById('fourth').innerHTML= b.length;
- 
- if(a.length > b.length)
-  {
-    var count = b.length - 1;
+    var count = b.length;
+    console.log("count" + count)
     document.getElementById('fifth').innerHTML = count;
      var res = ' ';
-     while(count)
+     while(count != 0)
      {
-        res += a.concat(a);
-        alert(res);
-      count--;
-     }
-     document.getElementById('seventh').innerHTML = res;
-
- }
-  else
-  { 
-    var count = a.length - 1;
-    document.getElementById('fifth').innerHTML = count;
-	  var res = ' ';
-     while(count)
-     {
-        res += a.concat(a);
-        //alert(res);
-      count--;
+        res += a;
+        console.log("result is: " + res);
+        count--;
      }
      document.getElementById('seventh').innerHTML = res;
   }
-}
 
 function reset(){
   location.reload();
-  document.getElementById('first').value = ""
-  document.getElementById('second').value = ""
+  // document.getElementById('first').value = ""
+  // document.getElementById('second').value = ""
  
 } 
