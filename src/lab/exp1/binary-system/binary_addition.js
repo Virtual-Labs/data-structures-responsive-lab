26d7edd4829b9138969fce7d848e75c16104f28f
khushpreet-kaur <khushpreetk363@gmail.com>
2015-11-25 13:46:43 +0530

    Implement following in binary multiplication:
    - Implement binary multiplication functionality, user will be able to multiply
      two binary numbers.
    - Animation to show the actual functionality of binary multiplication
    - TODO: alignment and styles to both the result divs for binary addition and
      multiplication

--- src/lab/exp1/binary-system/binary_addition.js
+++ src/lab/exp1/binary-system/binary_addition.js
@@ -49,7 +49,8 @@ function add_digit() {
       res = newres;
     }
     $('#result').prepend(res);
-    $('#carry').html(carry);
+
+    $('#carry').html('Carry: ' + carry);
     cur_digit -= 1;
     if(cur_digit >= 0) {
       move_arrow();
@@ -92,5 +93,71 @@ function move_arrow() {
   });
 }
 
-function mul(){
+var count, interm_muls;
+
+function mul() {
+  // $( "#addition" ).hide();  
+  // document.getElementById("addition" ).className = hide;
+
+  interm_muls = [];
+  a = $('#ip-num1').val();
+  b = $('#ip-num2').val();
+  console.log(a, b);
+  $('#firstValue').html("First number: " + a);
+  $('#secondValue').html("First number: " + b);
+  $('#secondValue').append('<hr>')
+  //console.log(b.length)
+  mul_logic(b.length);
 }
+
+function mul_logic(i) {
+  if(i == 0) {
+    finalRes();
+    return;
+  }
+  var res = "";
+  // console.log('iteration', i);
+  // console.log('array index', i-1);
+  var digit = b[i-1];
+  // console.log("digit is: ", + digit);
+
+
+  if(digit == 0) {
+    // console.log("Digit is",+ some)
+    // console.log("a.lenght", + a.length);
+    //"0".repeat(a.length);
+    res = makeZeros(a.length);
+    // console.log('res is', res);
+  }    
+  else {
+    res = a;
+    // console.log('res is', res);
+  }
+  // append zeros at the end for the position at the multiplication
+  var no_of_zeros = b.length - i;
+  res += makeZeros(no_of_zeros);
+  console.log('final res', res);
+  // append the intermediate mul to the display
+  $('#mul').append(res + '<br>');
+  // also push it to an array for final addition later
+  interm_muls.push(res);
+  // call itself again after 2 secs
+  setTimeout(function () {
+    mul_logic(i-1);
+  }, 2000);
+}
+
+function finalRes() {
+  var res = "0";
+  for(var i in interm_muls) {
+    res = add_binary_string(res, interm_muls[i]);
+  }
+  console.log('final final res', res);
+  $('#mul').append('<hr>');
+  $('#mul').append(res + '<br>');
+}
+ 
+function makeZeros(n) {
+  return "0".repeat(n);
+}
+    
