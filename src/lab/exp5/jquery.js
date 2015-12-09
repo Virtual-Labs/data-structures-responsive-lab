2733b41e953ce52799de369cd8c4f74e7f49c60c
Sravanthi MOdepu <sravanthimodepu@gmail.com>
2015-12-09 12:24:31 -0800

    modified exp5

--- src/lab/exp5/jquery.js
+++ src/lab/exp5/jquery.js
@@ -1,25 +1,61 @@
+   // $(document).ready(function(){
+   //   $("#order").hide();
+
+   //   $("#infix").click(function(){
+   //      $("#order").show();
+
+
  $(document).ready(function(){
      $("#hover1").hide();
-      $("#").hide();
+      $("#one").hide();
+      $("#in").show();
+      $("post").show();
+     
      $("#pre").click(function(){
          $("#hover1").show();
-         $("#").show();
+         $("#one").show();
+          $("#in").hide();
+         $("#post").hide();
      });
      
  });
   $(document).ready(function(){
      $("#hover1").hide();
+      $("#two").hide();
+      $("#pre").show();
+      $("post").show();
 
      $("#in").click(function(){
          $("#hover1").show();
+         $("#two").show();
+         $("#pre").hide();
+         $("#post").hide();
      });
      
  });
    $(document).ready(function(){
      $("#hover1").hide();
+     $("#three").hide();
+     $("#pre").show();
+     $("in").show();
 
      $("#post").click(function(){
          $("#hover1").show();
+         $("#three").show();
+         $("#in").hide();
+         $("#pre").hide();
      });
      
  });
+
+   $(document).ready(function(){
+    $("infix").click(function(){
+        $("text").val("Give Expression =>+");
+    });
+});
+
+//    $(document).ready(function(){
+//     $("keyC").click(function(){
+//         $("myDiv").empty();
+//     });
+// });
